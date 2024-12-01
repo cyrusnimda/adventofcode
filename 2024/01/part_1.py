58a2f54410be4f7664ce64d4154f83da7214ece4
Josu Ruiz <cyrusnimda@gmail.com>
2024-12-01 11:17:51 +0000

    Refactor list of arrays in part1

--- 2024/01/part_1.py
+++ 2024/01/part_1.py
@@ -1,29 +1,18 @@
-
-#a = [list(map(int, line.split())) for line in open('demo.txt').read().split("\n")]
-
 # a = []
 # b = []
 # for line in open('demo.txt').read().split("\n"):
-
 #     line_array = list(map(int, line.split()))
 #     a.append(line_array[0])
 #     b.append(line_array[1])
 
-# print(a)
-# print(b)
 
 #file = "demo.txt"
 file = "live.txt"
 
-a, b = zip(*[list(map(int, line.split())) for line in open(file).read().split("\n") if line])
-a = list(a)
-b = list(b)
+a, b = map(list, zip(*[list(map(int, line.split())) for line in open(file).read().split("\n") if line]))
 
 a.sort()
 b.sort()
-
-print(a)
-print(b)
 
 c = list(map(lambda x, y: abs(x - y), a, b))
 print(sum(c))
--- 2024/01/part_2.py
+++ 2024/01/part_2.py
@@ -2,15 +2,10 @@
 #file = "demo.txt"
 file = "live.txt"
 
-a, b = zip(*[list(map(int, line.split())) for line in open(file).read().split("\n") if line])
-a = list(a)
-b = list(b)
+a, b = map(list, zip(*[list(map(int, line.split())) for line in open(file).read().split("\n") if line]))
 
 a.sort()
 b.sort()
-
-print(a)
-print(b)
 
 c = list(map(lambda x, y: (x * b.count(x)), a, b))
 print(sum(c))
