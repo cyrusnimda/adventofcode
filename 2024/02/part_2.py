dbb71327c84a9b8683a7f8a24b11ca977e16d54e
Josu Ruiz <cyrusnimda@gmail.com>
2024-12-02 15:18:37 +0000

    Fix issue with part2

--- 2024/02/part_2.py
+++ 2024/02/part_2.py
@@ -6,10 +6,8 @@ reports = [list(map(int, line.split())) for line in open(file).read().split("\n"
 def is_report_safe(report):
     is_asc = (report[0] < report[1])
     is_safe = True
-    #print(report, is_asc)
     for i in range(len(report)-1):
         difference = report[i] - report[i+1]
-        #print(difference)
         if(abs(difference) > 3):
             is_safe = False
             return i+1
@@ -19,24 +17,28 @@ def is_report_safe(report):
         if(not is_asc and difference <= 0):
             is_safe = False
             return i+1
-    #print(is_safe)
     return is_safe
 
 total_safe_reports = 0
 for report in reports:
-    #print(report)
-    #print(is_report_safe(report))
     response = is_report_safe(report)
-    if response == True:
+    if type(response) == type(True):
         total_safe_reports += 1
         continue
 
-    del report[response]
-    #print("new ", report)
-    
-    #print(is_report_safe(report))
-    if is_report_safe(report)==True:
+    reportCopy1 = report.copy()
+    reportCopy2 = report.copy()
+    reportCopy3 = report.copy()
+
+    # delete element in position response
+    del reportCopy1[response]
+    del reportCopy2[0]
+    del reportCopy3[response-1]
+
+    if type(is_report_safe(reportCopy1)) == type(True) or type(is_report_safe(reportCopy2)) == type(True) or type(is_report_safe(reportCopy3)) == type(True):
         total_safe_reports += 1
+    else:
+        print(report)
 
     
 print(total_safe_reports)
