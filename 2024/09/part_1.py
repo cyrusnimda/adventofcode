5ae09d0497d15442815a3336c813a0d24c52210b
Josu Ruiz <cyrusnimda@gmail.com>
2025-08-28 22:13:06 +0100

    Add 2024 day 9 part1

--- 2024/09/part_1.py
+++ 2024/09/part_1.py
@@ -1,60 +1,37 @@
 import re
+import os
 
-file = "demo.txt"
-#file = "live.txt"
+#file = "demo.txt"
+file = "live.txt"
 
-disk = list(map(int, open(file, "r").read()))
-#print(disk)
+disk = list(map(int, open(os.path.join(os.path.dirname(__file__), file), "r").read()))
+# print(disk)
 
-# create disk based on block size
-id_number = 0
-new_disk = []
-is_block_time = True
-for block in disk:
-    if is_block_time==True:
-        new_block = str(id_number)
-        id_number+=1
-        is_block_time = False
-    else:
-        new_block = '.'
-        is_block_time = True
-    for i in range(block):
-        new_disk.append(new_block)
+hard_drive = []
+k = 0
+for i in range(len(disk)):
+    block = disk[i]
+    for x in range(block):
+        if i % 2 == 0:
+            hard_drive.append(str(k))
+        else:
+            hard_drive.append('.')
+    if i % 2 == 0:
+        k += 1
 
-print(new_disk)
-exit()
+# print(hard_drive)
 
-# sort free space
-def swap_last_block(disk : str) -> list:
-    # get position of last not "."  caracter
+while True:
+    first_free_space_position = hard_drive.index('.')
     last_not_free_caracter_position = 0
-    for i in range(len(disk)-1, 0, -1):
-        if disk[i] != '.':
+    for i in range(len(hard_drive)-1, 0, -1):
+        if hard_drive[i] != '.':
             last_not_free_caracter_position = i
             break
-
-    first_free_space_position = disk.index('.')
-
-    disk = list(disk)
-    # replace first free space with last caracter
-    last_caracter = disk[last_not_free_caracter_position]
-    disk[first_free_space_position] = last_caracter
-
-    # add free space at the end
-    disk[last_not_free_caracter_position] = '.'
-
-    disk = "".join(disk)
-    return disk
-
-disk = "".join(new_disk)
-
-def is_disk_defragmented(disk: str) -> bool:
-    return re.search(r"^\d+\.+$", disk)
-
-
-while not is_disk_defragmented(disk):
-    disk = swap_last_block(disk)
-    #print(disk)
+    # exit if free space is later than last caracter
+    if first_free_space_position > last_not_free_caracter_position: break
+    hard_drive[first_free_space_position] = hard_drive[last_not_free_caracter_position]
+    hard_drive[last_not_free_caracter_position] = '.'
 
 
 def get_checksum(disk: str) -> int:
@@ -64,4 +41,4 @@ def get_checksum(disk: str) -> int:
         sum += i * int(disk[i])
     return sum
 
-print("Checksum:", get_checksum(disk))
+print("Checksum:", get_checksum(hard_drive))
