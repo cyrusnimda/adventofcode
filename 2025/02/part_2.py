8f07e7964316e9cd9238d34f59ac387000a07780
Josu Ruiz <cyrusnimda@gmail.com>
2025-12-02 15:25:16 +0000

    Add 2025 part 2

--- 2025/02/part_2.py
+++ 2025/02/part_2.py
@@ -1,5 +1,5 @@
 
-file = "demo.txt"
+file = "live.txt"
 
 movements_raw =  open(file).read().split(",")
 movements = []
@@ -7,23 +7,34 @@ for move in movements_raw:
     first_id, last_id = move.split("-")
     movements.append(( int(first_id), int(last_id) ))
 
+
+def is_invalid_seat_id(seat_id: str) -> bool:
+    # print("Checking Seat ID:", seat_id)
+    chain_range = range(1, len(seat_id))
+    # print("Chain Range:", list(chain_range))
+    for index in chain_range:
+        substring = seat_id[:index]
+        substring_length = int(len(substring))
+        # print("  Checking Substring:", substring)
+        if substring_length % substring_length != 0:
+            # print(int(seat_id), substring_length)
+            # print("    Not Divisible, Continuing")
+            continue
+        
+        times_to_repeat = len(seat_id) // substring_length
+        # print("    Times to Repeat:", times_to_repeat)
+        repeated = substring * times_to_repeat
+        # print("    Repeated:", repeated)
+        if repeated == seat_id:
+            return True
+    return False
+
 invalid_ids = set()
 for first_id, last_id in movements:
-    for seat_id in range(first_id, last_id + 1):
+    for seat_id in range(first_id, last_id+1):
         current_id = str(seat_id)
-        # check is even number
-        if len(current_id) % 2 !=0:
-            continue
-
-        half_length = len(current_id) // 2
-        left_half = current_id[:half_length]
-        right_half = current_id[half_length:]
-        if left_half == right_half:
+        if is_invalid_seat_id(current_id):
             invalid_ids.add(seat_id)
-            
-        #print(f"Invalid Seat ID: {current_id}")
-    #exit()
-
-#print("Invalid IDs:", invalid_ids)
 
 print("Total Invalid IDs:", sum(invalid_ids))
+
